c50ac9db855758ed1ba6adc8a6cd9be6180624d6
Benjamin Hand <gneeshot@gmail.com>
2015-06-23 15:11:35 -0500

    adding in some fields, defaults, null: falses to tables

--- db/migrate/20150623200759_create_cheeses.rb
+++ db/migrate/20150623200759_create_cheeses.rb
@@ -1,11 +1,11 @@
 class CreateCheeses < ActiveRecord::Migration
   def change
     create_table :cheeses do |t|
-      t.string :name
-      t.string :kind
-      t.integer :rating
-      t.text :description
-      t.string :pic
+      t.string :name, null: false, default: 'cheese'
+      t.string :kind, null: false, default: 'american'
+      t.integer :rating, null: false, default: 0
+      t.text :description, null: false, default: 'cheesy'
+      t.string :pic, null: false, default: 'http://cancats.net/wp-content/uploads/2014/07/can-cats-eat-cheese-main.jpg'
       t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps null: false
